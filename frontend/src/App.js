aa826769786473741d77783e8bfa735312d380a3
Aneja <91323255+AnejaPirnat@users.noreply.github.com>
2025-05-08 15:11:39 +0200

    PR-57 Changed routes and added assets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,10 @@ import Index from "./components/Index"
 import Register from "./components/Register";
 import Profile from "./components/Profile";
 import Logout from "./components/Logout";
+<<<<<<< Updated upstream
+=======
+import Generate from "./components/GenerateShow";
+>>>>>>> Stashed changes
 
 
 
@@ -55,6 +59,10 @@ function App() {
                 <Route path="/register" element={<Register />} />                
                 <Route path="/profile" element={<Profile />} />
                 <Route path="/logout" element={<Logout />} />
+<<<<<<< Updated upstream
+=======
+                <Route path="/generate" element={<Generate />} />
+>>>>>>> Stashed changes
               </Routes>
             </main>
 
