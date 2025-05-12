14e22e85e9947c46491c17b9bdd6be50c0d9b3d6
Aneja <91323255+AnejaPirnat@users.noreply.github.com>
2025-05-12 22:18:50 +0200

    Dodan css za formo

--- frontend/src/components/AddArticles.js
+++ frontend/src/components/AddArticles.js
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import addIcon from "../assets/plus-white.svg";
+import removeIcon from "../assets/x-white.svg";
+import submitIcon from "../assets/submit-white.svg";
 
 const MAX_ROWS = 10;
 const MIN_ROWS = 1;
@@ -36,7 +39,7 @@ const AddArticles = () => {
     );
 
     if (hasEmptyField) {
-      alert("Please fill out all fields for every ingredient.");
+      alert("Prosimo, izpolnite vsa polja za vsako sestavino.");
       return;
     }
 
@@ -54,10 +57,9 @@ const AddArticles = () => {
     setLoading(false);
 
     if (res.status === 201) {
-      console.log(res);
       window.location.replace("/myfridge");
     } else {
-      console.log(res);
+      console.log("Napaka pri vnosu sestavin:", res);
     }
   };
 
@@ -79,100 +81,119 @@ const AddArticles = () => {
           }}
         >
           <div className="spinner-border text-light" role="status">
-            <span className="visually-hidden">Loading...</span>
+            <span className="visually-hidden">Nalaganje...</span>
           </div>
         </div>
       )}
 
-      <h1 style={{ color: "rgb(163,188,4)" }}>Hello!</h1>
-      <p
-        style={{
-          color: "rgb(163,188,4)",
-          fontFamily: "Marker Felt, fantasy",
-          fontSize: 24,
-        }}
-      >
-        Let's generate a recipe together!
-      </p>
-      <p style={{ color: "rgb(163,188,4)" }}>
-        • Enter the name and weight of each ingredient you have in your pantry.
-        <br />
-        • Click "Submit" to generate a recipe.
-        <br />
-        • Follow and rate the recipe, and optionally share it.
-        <br />
-      </p>
-      <form onSubmit={handleSubmit}>
-        <h2 style={{ color: "rgb(163,188,4)" }}>Enter your ingredients!</h2>
-        <div id="form-container">
-          {ingredients.map((ingredient, index) => (
-            <div className="form-row one-ingri" key={index}>
-              <div className="form-group col-md-10">
-                <label style={{ color: "rgb(163,188,4)" }}>Ingredient</label>
+      <div style={{ textAlign: "center", marginBottom: "10px" }}>
+        <h1 style={{ color: "#333", fontSize: "2rem" }}>Napolni svoj hladilnik!</h1>
+        <div style={{ fontSize: "1.6rem", color: "#333" }}>
+          Vnesi vse sestavine, ki jih imaš trenutno doma
+        </div>
+      </div>
+
+          <form onSubmit={handleSubmit}>
+      <div id="form-container" className="d-flex flex-column align-items-center">
+        {ingredients.map((ingredient, index) => (
+          <div className="form-row one-ingri mb-3 w-75" key={index}>
+            <div className="form-group mb-2">
+              <label style={{ color: "#333" }}>Ime sestavine</label>
+              <input
+                type="text"
+                className="form-control"
+                value={ingredient.name}
+                onChange={(e) =>
+                  handleInputChange(index, "name", e.target.value)
+                }
+              />
+            </div>
+            <div className="form-group">
+              <label style={{ color: "#333" }}>Teža / količina</label>
+              <div className="input-group">
                 <input
-                  type="text"
+                  type="number"
                   className="form-control"
-                  value={ingredient.name}
+                  value={ingredient.quantity}
                   onChange={(e) =>
-                    handleInputChange(index, "name", e.target.value)
+                    handleInputChange(index, "quantity", e.target.value)
                   }
                 />
+                <select
+                  className="form-control"
+                  value={ingredient.unit}
+                  onChange={(e) =>
+                    handleInputChange(index, "unit", e.target.value)
+                  }
+                >
+                  <option value="mg">mg</option>
+                  <option value="g">g</option>
+                  <option value="kg">kg</option>
+                  <option value="l">l</option>
+                  <option value="ml">ml</option>
+                  <option value="pc">kos</option>
+                </select>
               </div>
-              <div className="form-group col-md-10">
-                <label style={{ color: "rgb(163,188,4)" }}>
-                  Weight/quantity
-                </label>
-                <div className="input-group">
-                  <input
-                    type="number"
-                    className="form-control"
-                    value={ingredient.quantity}
-                    onChange={(e) =>
-                      handleInputChange(index, "quantity", e.target.value)
-                    }
-                  />
-                  <select
-                    className="form-control"
-                    value={ingredient.unit}
-                    onChange={(e) =>
-                      handleInputChange(index, "unit", e.target.value)
-                    }
-                  >
-                    <option value="mg">mg</option>
-                    <option value="g">g</option>
-                    <option value="kg">kg</option>
-                    <option value="l">l</option>
-                    <option value="ml">ml</option>
-                    <option value="pc">pc</option>
-                  </select>
-                </div>
-              </div>
-              <hr />
             </div>
-          ))}
-        </div>
-        <div className="form-group mt-2">
-          <button
-            type="button"
-            className="btn btn-primary me-2"
-            onClick={addRow}
-            disabled={ingredients.length >= MAX_ROWS}
-          >
-            Add more
-          </button>
-          <button
-            type="button"
-            className="btn btn-danger me-2"
-            onClick={removeRow}
-            disabled={ingredients.length <= MIN_ROWS}
-          >
-            Remove
-          </button>
-          <button type="submit" className="btn btn-success">
-            Submit
-          </button>
-        </div>
-      </form>
+            <hr />
+          </div>
+        ))}
+      </div>
+
+      <div className="form-group mt-3 text-center d-flex justify-content-center gap-2 flex-wrap">
+        <button
+          type="button"
+          style={{
+            backgroundColor: "#add8e6",
+            border: "none",
+            color: "white",
+            display: "flex",
+            alignItems: "center",
+            padding: "8px 16px",
+            borderRadius: "6px",
+          }}
+          onClick={addRow}
+          disabled={ingredients.length >= MAX_ROWS}
+        >
+          <span style={{ fontSize: "1rem", marginRight: "10px" }}>Dodaj vrstico</span>
+          <img src={addIcon} alt="Dodaj" style={{ width: "24px", height: "24px" }} />
+        </button>
+
+        <button
+          type="button"
+          style={{
+            backgroundColor: "#e84138",
+            border: "none",
+            color: "white",
+            display: "flex",
+            alignItems: "center",
+            padding: "8px 16px",
+            borderRadius: "6px",
+          }}
+          onClick={removeRow}
+          disabled={ingredients.length <= MIN_ROWS}
+        >
+          <span style={{ fontSize: "1rem", marginRight: "10px" }}>Odstrani vrstico</span>
+          <img src={removeIcon} alt="Odstrani" style={{ width: "24px", height: "24px" }} />
+        </button>
+
+        <button
+          type="submit"
+          style={{
+            backgroundColor: "#b0d16b",
+            border: "none",
+            color: "white",
+            display: "flex",
+            alignItems: "center",
+            padding: "8px 16px",
+            borderRadius: "6px",
+          }}
+        >
+          <span style={{ fontSize: "1rem", marginRight: "10px" }}>Vnesi</span>
+          <img src={submitIcon} alt="Vnesi" style={{ width: "24px", height: "24px" }} />
+        </button>
+      </div>
+    </form>
     </div>
   );
 };
